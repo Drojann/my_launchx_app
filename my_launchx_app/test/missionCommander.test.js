826526d89ed5808d5e25d1cf0b073f07632e1c52
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2022-05-12 22:28:01 -0500

    proyect_1

---
+++ my_launchx_app/test/missionCommander.test.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./../app/missionCommander');
+
+describe("unit test for missionCommander", () => {
+    test('1) Create a mission commander objet', () => {
+    const myMissionCommander = new MissionCommander("Woopa")
+    expect(myMissionCommander.name).toBe("Woopa");
+    });
+  })
+  
